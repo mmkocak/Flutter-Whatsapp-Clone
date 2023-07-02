c85ee9c2e763419652f03054a0e89ce28ae44209
Mirze Muhammet Koçak <muhammetlocak774@gmail.com>
2023-07-02 16:00:26 +0300

    2. commit

---
+++ lib/feature/welcome/pages/welcome_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/utils/colors.dart';
+import 'package:whatsapp_clone/common/utils/widgets/custom_elevated_button.dart';
+import 'package:whatsapp_clone/feature/welcome/widgets/language_button.dart';
+import 'package:whatsapp_clone/feature/welcome/widgets/privacy_and_terms.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(
+        children: [
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                child: Image.asset(
+                  "assests/images/circle.png",
+                  color: Coloors.greenDark,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "Welcome to Whatsapp",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const PrivacyAndTerms(),
+                CustomElevatedButton(onPressed: (){}, text: "AGREE AND CONTİNUE",),
+                const SizedBox(
+                  height: 50,
+                ),
+                const LanguageButton(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
